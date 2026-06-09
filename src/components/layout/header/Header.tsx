5fe11e302a955b81c2bd9ea3ca9b1220bd047d8e
asanrustamov49-netizen <asanrustamov49@gmail.com>
2026-06-09 22:32:47 +0600

    admin, login did

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -4,13 +4,14 @@ import Link from "next/link";
 import scss from "./header.module.scss";
 import { IoSearchOutline } from "react-icons/io5";
 import { FiUser } from "react-icons/fi";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState, useRef, useEffect } from "react";
 
 const Header = () => {
   const [isLogged, setIsLogged] = useState<boolean>(false);
   const pathname = usePathname();
+  const { push } = useRouter();
 
   const navRef = useRef<HTMLDivElement>(null);
   const underlineRef = useRef<HTMLSpanElement>(null);
@@ -18,58 +19,58 @@ const Header = () => {
   useEffect(() => {
     const activeLink = navRef.current?.querySelector(
       "[data-active='true']",
-    ) as HTMLElement;
+    ) as HTMLElement | null;
 
     if (activeLink && underlineRef.current) {
-      underlineRef.current.style.width = activeLink.offsetWidth + "px";
-      underlineRef.current.style.transform = `translateX(${activeLink.offsetLeft}px)`;
+      const underline = underlineRef.current;
+
+      underline.style.opacity = "1";
+      underline.style.width = activeLink.offsetWidth + "px";
+      underline.style.transform = `translateX(${activeLink.offsetLeft}px)`;
+    } else if (underlineRef.current) {
+      underlineRef.current.style.opacity = "0";
     }
   }, [pathname]);
+  const handleLogin = () => {
+    push("/login");
+    setIsLogged(true);
+  };
+
 
   return (
     <header className={scss.container}>
       <div className="container">
         <div className={scss.mainContainer}>
           <img src="./image 1.png" alt="logo" />
-
-          {/* NAV */}
           <nav className={scss.nav} ref={navRef}>
             <Link href="/" data-active={pathname === "/"}>
               HOME
             </Link>
-
             <Link href="/about" data-active={pathname === "/about"}>
               ABOUT
             </Link>
-
             <Link href="/tours" data-active={pathname === "/tours"}>
               TOURS
             </Link>
-
             <Link href="/contacts" data-active={pathname === "/contacts"}>
               CONTACTS
             </Link>
-
             <Link href="/admin" data-active={pathname === "/admin"}>
               ADMIN
             </Link>
-
             <span ref={underlineRef} className={scss.underline}></span>
           </nav>
-
-          {/* RIGHT SIDE */}
           <div className={scss.sign}>
             <span>
               <IoSearchOutline />
             </span>
-
             <span>
               <FiUser />
             </span>
-
             <AnimatePresence>
               {!isLogged && (
                 <motion.button
+                  onClick={() => handleLogin()}
                   className={scss.signBtn}
                   initial={{ opacity: 0, y: -10 }}
                   animate={{ opacity: 1, y: 0 }}
---
+++ src/components/pages/login/Login.tsx
@@ -0,0 +1,106 @@
+"use client";
+import scss from "./login.module.scss";
+import { FiUser } from "react-icons/fi";
+import { CiLock } from "react-icons/ci";
+import { FaCircle } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+interface IForm {
+  usename: string;
+  password: number;
+}
+
+const Login = () => {
+  const [userName, setUserName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const { handleSubmit, reset, register } = useForm<IForm>();
+  const disabled: any = {
+    userName,
+    password,
+  };
+  const handleData = (data: IForm) => {
+    reset();
+  };
+  return (
+    <div className={scss.container}>
+      <div className="container">
+        <div className={scss.mainContainer}>
+          <div className={scss.mainLogin}>
+            <div className={scss.info}>
+              <h1>LOGIN</h1>
+              <h4>Welcome Back to Our Site</h4>
+            </div>
+            <form onSubmit={handleSubmit(handleData)} className={scss.inp}>
+              <div className={scss.username}>
+                <input
+                  {...register("usename")}
+                  onChange={(e) => setUserName(e.target.value)}
+                  type="text"
+                  placeholder="Username or Email"
+                />
+                <span>
+                  <FiUser />
+                </span>
+              </div>
+              <div className={scss.password}>
+                <input
+                  {...(register("password"), { required: true })}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="text"
+                  placeholder="Password"
+                />
+                <span>
+                  <CiLock />
+                </span>
+              </div>
+              <div className={scss.h5}>
+                <h5>Forgot Password?</h5>
+                <button
+                  style={{
+                    cursor: disabled.userName ? "pointer" : "not-allowed",
+                  }}
+                  disabled={!disabled}
+                  type="submit"
+                >
+                  LOG IN
+                </button>
+              </div>
+            </form>
+            <div className={scss.images}>
+              <p>______________</p>
+              <span>
+                <FaCircle />
+              </span>
+              <span className={scss.center}>
+                <FaCircle />
+              </span>
+              <span>
+                <FaCircle />
+              </span>
+              <p>______________</p>
+            </div>
+            <div className={scss.login}>
+              <p>
+                Don`t have an account? <span>Sign Up Now</span>
+              </p>
+              <h6>Or log in with:</h6>
+              <div className={scss.icons}>
+                <span>
+                  <FaGoogle />
+                </span>
+                <span>
+                  <FaFacebook />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
